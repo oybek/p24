0a6101dfa5f4770ff83a294175a537d1056724ec
Oibek Khashimov <o.khashimov@eslgaming.com>
2024-11-28 15:31:35 +0600

    cosmetic updates

--- telegram/handle_voice.go
+++ telegram/handle_voice.go
@@ -3,6 +3,7 @@ package telegram
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -13,6 +14,8 @@ import (
 	"github.com/oybek/choguuket/model"
 	"github.com/samber/lo"
 	"github.com/sashabaranov/go-openai"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 func (lp *LongPoll) handleVoice(b *gotgbot.Bot, ctx *ext.Context) error {
@@ -22,6 +25,8 @@ func (lp *LongPoll) handleVoice(b *gotgbot.Bot, ctx *ext.Context) error {
 	if voice.Duration > 20 {
 		return lp.sendText(chat.Id, TextTooLongVoice)
 	}
+
+	lp.sendText(chat.Id, "Ищу подходящие аптеки")
 
 	text, err := lp.transcribeVoice(voice)
 	if err != nil {
@@ -39,7 +44,10 @@ func (lp *LongPoll) handleVoice(b *gotgbot.Bot, ctx *ext.Context) error {
 
 	for _, tuple := range tuples {
 		text, _ := toMessage(tuple)
-		lp.sendText(chat.Id, text)
+		err := lp.sendText(chat.Id, text)
+		if err != nil {
+			log.Printf("error sending message: %s", err.Error())
+		}
 		time.Sleep(2 * time.Second)
 	}
 
@@ -50,13 +58,6 @@ func (lp *LongPoll) handleVoice(b *gotgbot.Bot, ctx *ext.Context) error {
 		)
 	}
 	return nil
-}
-
-func toMessage(t lo.Tuple2[model.Apteka, []string]) (string, *gotgbot.SendMessageOpts) {
-	a, ms := t.A, t.B
-	baseInfo := fmt.Sprintf("Аптека: %s\nАдрес: %s\nPhone: %s", a.Name, a.Address, a.Phone)
-	presenceInfo := fmt.Sprintf("В наличии: %s", strings.Join(ms, ", "))
-	return baseInfo + "\n\n" + presenceInfo, &gotgbot.SendMessageOpts{}
 }
 
 func (lp *LongPoll) searchApteka(medicineNames []string) ([]lo.Tuple2[model.Apteka, []string], error) {
@@ -101,4 +102,17 @@ func (lp *LongPoll) transcribeVoice(voice *gotgbot.Voice) (string, error) {
 	}
 
 	return openaiResp.Text, nil
+}
+
+func toMessage(t lo.Tuple2[model.Apteka, []string]) (string, *gotgbot.SendMessageOpts) {
+	a, ms := t.A, t.B
+	baseInfo := fmt.Sprintf(
+		"%s %s\n%s %s\n%s %s",
+		EmojiHospital, cases.Title(language.Und).String(a.Name),
+		EmojiPin, cases.Title(language.Und).String(a.Address),
+		EmojiPhone, a.Phone,
+	)
+	presenceInfo := fmt.Sprintf("В наличии: %s", strings.Join(ms, ", "))
+
+	return baseInfo + "\n\n" + presenceInfo, &gotgbot.SendMessageOpts{}
 }
